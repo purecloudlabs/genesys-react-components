b384eca438a7957eb356140142951fd69545d65f
Tim Smith <tim.j.smith@genesys.com>
2022-09-07 10:10:08 -0600

    DEVENGAGE-1400 remove node-sass

--- rollup.config.js
+++ rollup.config.js
@@ -9,11 +9,6 @@ const packageJson = require('./package.json');
 export default {
 	input: 'src/index.ts',
 	output: [
-		// {
-		// 	file: packageJson.main,
-		// 	format: 'cjs',
-		// 	sourcemap: true,
-		// },
 		{
 			file: packageJson.module,
 			format: 'esm',
