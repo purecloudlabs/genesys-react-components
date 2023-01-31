5f401064745fed38c8f143df32e0f951d317d7d9
Zino Onokpise <Zino.Onokpise@genesys.com>
2023-01-31 11:51:48 -0600

    devengage-1574 import from cjs

--- src/codefence/CodeFence.tsx
+++ src/codefence/CodeFence.tsx
@@ -1,7 +1,9 @@
 import { GenesysDevIcon, GenesysDevIcons } from 'genesys-dev-icons';
 import React, { useState } from 'react';
+// import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
 import { PrismAsync as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism/index.js';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/cjs/styles/prism/index.js';
+// import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism/index.js';
 
 import CopyButton from '../copybutton/CopyButton';
 
