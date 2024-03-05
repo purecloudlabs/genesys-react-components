bd4ae18b922cacb909076c8fe8f50e41cc1ca1c9
Zino Onokpise <Zino.Onokpise@genesys.com>
2024-03-05 01:02:17 -0600

    DEVENGAGE-2422 improved tab identification

--- src/dxtabbedcontent/DxTabbedContent.tsx
+++ src/dxtabbedcontent/DxTabbedContent.tsx
@@ -5,31 +5,25 @@ import './DxTabbedContent.scss';
 
 export default function DxTabbedContent(props: DxTabbedContentProps) {
 	const [activeTab, setActiveTab] = useState(props.initialTabId || 0);
-	const [titles, setTitles] = useState<React.ReactNode[]>(
-		// Scrape titles from child elements
-		React.Children.toArray(props.children).map((child: any) => {
-			if (!child || !child.props || !child.props.title) return 'Unknown title';
-			return child.props.title;
-		})
-	);
-
-	useEffect(() => {
-		setTitles(
-			React.Children.toArray(props.children).map((child: any) => {
-				if (!child || !child.props || !child.props.title) return 'Unknown title';
-				return child.props.title;
-			})
-		);
-	}, [props.children]);
 
 	return (
 		<div className={`dx-tabbed-content${props.className ? ' ' + props.className : ''}`}>
 			<div className="tab-titles">
-				{titles.map((title, i) => (
-					<span key={i} className={`tab-title${i === activeTab ? ' active' : ''}`} onClick={() => setActiveTab(i)}>
-						{title}
-					</span>
-				))}
+				{React.Children.toArray(props.children).map((child: any, i) => {
+					if (!child) return;
+					if (!child.props || !child.props.title) {
+						return (
+							<span key={i} className={`tab-title${i === activeTab ? ' active' : ''}`} onClick={() => setActiveTab(i)}>
+								{'Unknown title'}
+							</span>
+						);
+					}
+					return (
+						<span key={i} className={`tab-title${i === activeTab ? ' active' : ''}`} onClick={() => setActiveTab(i)}>
+							{child.props.title}
+						</span>
+					);
+				})}
 			</div>
 			<div className="tab-content">{React.Children.toArray(props.children)[activeTab]}</div>
 		</div>
