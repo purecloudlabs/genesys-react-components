38ea9f9e4a2ce050dc301d1ee098f3a2d24e1929
Jacob Shaw <jacob.shaw@genesys.com>
2025-10-02 15:07:32 -0500

    update

--- src/dxtextbox/DxTextbox.tsx
+++ src/dxtextbox/DxTextbox.tsx
@@ -5,7 +5,7 @@ import { DxTextboxProps } from '..';
 
 import './DxTextbox.scss';
 
-const dateMmDdYyyy = /^\d{2}\/\d{2}\/\d{4}$/;
+// const dateMmDdYyyy = /^\d{2}\/\d{2}\/\d{4}$/;
 const dateYyyyMmDd = /^\d{4}-\d{2}-\d{2}$/;
 
 export default function DxTextbox(props: DxTextboxProps) {
@@ -120,13 +120,21 @@ export default function DxTextbox(props: DxTextboxProps) {
 	};
 
 	const parseDate = (input: string) => {
-		// if (!input || !input.match(/^\d{2}\/\d{2}\/\d{4}$/)) return;
-		// const parts: string[] = input.split('/');
-		// const month = parts[0];
-		const date = new Date(input);
-		let offset = date.getTimezoneOffset() * 60000;
-		let utcDate = new Date(date.getTime() + offset);
-		return isNaN(utcDate.getTime()) ? null : utcDate;
+		if (!input) return;
+		let date;
+		if (input.match(dateYyyyMmDd)) {
+			/* 
+			 * This date format causes the Date constructor to treat it as a UTC date, whereas the other formats are parsed as local dates.
+			 * This causes the local timezone offset to be subtracted from the UTC date time,
+			 * and the result is that the parsed date is a day behind the date selected.
+			 */
+			date = new Date(input);
+			let offset = date.getTimezoneOffset() * 60000;
+			date = new Date(date.getTime() + offset);
+		} else {
+			date = new Date(input);
+		}
+		return isNaN(date.getTime()) ? null : date;
 	};
 
 	// Normalize input type
